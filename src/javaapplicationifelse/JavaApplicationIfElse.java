07dc5213257c356f7bba09796d46655bb5d0ef0a
Domicio <testedomicio@hotmail.com>
2015-10-25 18:18:19 -0200

    Ifelse
    if e Else l e ll

---
+++ src/javaapplicationifelse/JavaApplicationIfElse.java
@@ -0,0 +1,39 @@
+package javaapplicationifelse;
+
+import javax.swing.JOptionPane;
+
+public class JavaApplicationIfElse {
+
+    public static void main(String[] args) {
+
+        /*int numero = 1;
+        
+        
+         if (numero == 0)
+        
+         JOptionPane.showMessageDialog(null, " É igual Zero");
+         else 
+         JOptionPane.showMessageDialog(null, "Não igual Zero"); 
+        
+        boolean valido = true;
+
+        if (! valido) {
+            
+            JOptionPane.showMessageDialog(null, "Não É valido");
+        } else {
+            JOptionPane.showMessageDialog(null, "É valido");*/
+        String hoje = "Terça";
+        
+         if (hoje == "Segunda") {
+            
+            System.out.println("A condição (hoje = segunda) foi verdadeira");
+        } else {
+            System.out.println("A condição (hoje = segunda) foi falsa");
+        
+        
+        
+        }
+
+    }
+
+}
